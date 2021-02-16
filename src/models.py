3f9f2b6d39c20a95aad9afbafcbfbc8d4d773218
Maria Eugenia <chacon.mariae.ucv@gmail.com>
2021-02-15 20:46:54 -0400

    add change for country code

--- src/models.py
+++ src/models.py
@@ -20,6 +20,7 @@ class User(db.Model):
     user_name=db.Column(db.String(20), unique=True, nullable=False)
     cedula_rif=db.Column(db.String(20))
     country=db.Column(db.String(120))
+    country_code=db.Column(db.String(3), unique=True, nullable=False)
     region_state=db.Column(db.String(120))
     municipality=db.Column(db.String(120))
     hashed_password=db.Column(db.String(120),unique=False, nullable=False)
@@ -43,6 +44,7 @@ class User(db.Model):
         self.user_name=body['user_name']
         self.cedula_rif=body['cedula_rif']
         self.country=body['country']
+        self.country_code['country_code']
         self.region_state=body['region_state']
         self.municipality=body['municipality']
         self.salt=b64encode(os.urandom(4)).decode("utf-8")
@@ -73,6 +75,7 @@ class User(db.Model):
             "user_name":self.user_name,
             "cedula_rif":self.cedula_rif,
             "country":self.country,
+            "country_code":self.country_code,
             "region_state":self.region_state,
             "municipality":self.municipality
         }
